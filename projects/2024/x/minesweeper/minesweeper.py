6ba9903e631759bc3d59b30480eeab3bc62ee82d
JacquesM13 <164623163+JacquesM13@users.noreply.github.com>
2024-09-11 10:46:02 +0100

    Update minesweeper.py

--- projects/2024/x/minesweeper/minesweeper.py
+++ projects/2024/x/minesweeper/minesweeper.py
@@ -213,7 +213,7 @@ class MinesweeperAI():
                 if 0 <= i < self.height and 0 <= j < self.width:
                     self.neighbours.add((i, j))
                     
-        self.knowledge.append(self.neighbours, count)
+        self.knowledge.append(f"{self.neighbours} = {count}")
         # raise NotImplementedError
 
     def make_safe_move(self):
