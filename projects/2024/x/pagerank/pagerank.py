805f25b6de6f776ff95442fb7739c2c77b64339f
JacquesM13 <164623163+JacquesM13@users.noreply.github.com>
2024-09-30 19:02:18 +0100

    Update pagerank.py

--- projects/2024/x/pagerank/pagerank.py
+++ projects/2024/x/pagerank/pagerank.py
@@ -145,16 +145,43 @@ def iterate_pagerank(corpus, damping_factor):
                 inbound_dict[key] = []
                 for page in corpus.keys():
                     inbound_dict[key].append(page)
-            
-                        
-        return_dict_copy = return_dict.copy()
         
-        for page in return_dict_copy:
-            for value in inbound_dict[page]:
-                return_dict[page] = damping_factor*(return_dict_copy[value]/len(corpus[value]))
-                return_dict[page] += (1-damping_factor)/len(corpus)
+        notConverged = True
+        
+        while notConverged:
             
-        print(return_dict.values())
+            return_dict_copy = return_dict.copy()
+            
+            for page in return_dict_copy:
+                print("page : " + page)
+                return_dict[page] = (1-damping_factor)/len(corpus)
+                for value in inbound_dict[page]:
+                    print("value: " + value)
+                    increment = damping_factor*(return_dict_copy[value]/len(corpus[value]))
+                    return_dict[page] += increment
+                    
+            for page in return_dict_copy:
+                if return_dict_copy[page] - 0.001 < return_dict[page] < return_dict_copy[page] + 0.001:
+                    notConverged = False
+                else:
+                    notConverged = True
+            
+            print(return_dict.values())
+            
+        return return_dict
+        
+        
+        # return_dict_copy = return_dict.copy()
+        
+        # for page in return_dict_copy:
+        #     print("page : " + page)
+        #     return_dict[page] = (1-damping_factor)/len(corpus)
+        #     for value in inbound_dict[page]:
+        #         print("value: " + value)
+        #         return_dict[page] += damping_factor*(return_dict_copy[value]/len(corpus[value]))
+        
+        # print(return_dict.values())
+        
         return return_dict
     
     except:
